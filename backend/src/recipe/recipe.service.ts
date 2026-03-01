036b97becc0ea28f82aa02b8e8b43c1ce212bb4d
Marco <mrd.olivieri@gmail.com>
2026-03-01 18:25:50 +0100

    schema modification with few addition and tests

--- backend/src/recipe/recipe.service.ts
+++ backend/src/recipe/recipe.service.ts
@@ -10,10 +10,29 @@ export class RecipesService {
     return await this.prisma.recipe.findMany();
   }
 
-  async create(data: { description: string }): Promise<Recipe> {
+  async create(data: {
+    description: string;
+    steps?: string[];
+    ingredients?: string[];
+  }): Promise<Recipe> {
+    // #TODO: Add steps and ingredients
     return await this.prisma.recipe.create({
       data: {
         description: data.description,
+        steps: {
+          create: {
+            description: 'Step 1',
+          },
+        },
+        recipeIngredients: {
+          create: {
+            ingredient: {
+              create: {
+                name: 'Ingredient 1',
+              },
+            },
+          },
+        },
       },
     });
   }
